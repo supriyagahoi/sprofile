b1a35b75a95c1ad738a29f6fc0877e2bcda7515c
supriyagahoi <gahoisupriya@gmail.com>
2021-06-01 22:25:12 +0530

    initial commit

---
+++ src/component/nav.js
@@ -0,0 +1,12 @@
+//import React, { Component } from 'react';
+import './nav.css'
+function nav() {
+    return (
+      <div className="App">
+
+       <h1 >jhadhvnm</h1>
+      </div>
+    );
+  }
+  
+  export default nav;
